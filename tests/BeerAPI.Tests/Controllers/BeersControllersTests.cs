eb20ee426ef697f8653c0f81cf4cd60518f9c4c5
Freddy Dordoni <freddy.dordoni@danmurphys.com.au>
2024-04-16 18:57:35 +1000

    added test for the Trolley Controller and the Beers controller

--- tests/BeerAPI.Tests/Controllers/BeersControllersTests.cs
+++ tests/BeerAPI.Tests/Controllers/BeersControllersTests.cs
@@ -1,12 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BeerAPI.Controllers;
+using BeerAPI.Models;
+using BeerAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
 
 namespace BeerAPI.Tests.Controllers
 {
-    internal class BeersControllersTests
+    public class BeersControllersTests
     {
+        private readonly Mock<IBeerService> _mockBeerService;
+        private readonly Mock<IBeerDescriptionService> _mockBeerDescriptionService;
+        private readonly BeersController _controller;
+
+        public BeersControllersTests()
+        {
+            _mockBeerService = new Mock<IBeerService>();
+            _mockBeerDescriptionService = new Mock<IBeerDescriptionService>();
+            _controller = new BeersController(_mockBeerService.Object, _mockBeerDescriptionService.Object);
+        }
+
+
+        [Fact]
+        public void GetBeerById_NonExistingId_ReturnsNotFoundResult()
+        {
+            // Arrange
+            _mockBeerService.Setup(service => service.GetBeerById(It.IsAny<int>())).Returns(default(Beer?));
+
+            // Act
+            var result = _controller.GetBeerById(999); // this ID doesn't exist
+
+            // Assert
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public void UpdatePromoPrice_ValidRequest_UpdatesPromoPrice()
+        {
+            // Arrange
+            var beerId = 1;
+            var promoPriceUpdateRequest = new PromoPriceUpdateRequest { NewPromoPrice = 4.99M };
+            var mockBeer = new Beer { Id = beerId, Name = "Lager", Price = 5.99M };
+
+            _mockBeerService.Setup(service => service.GetBeerById(beerId)).Returns(mockBeer);
+            _mockBeerService.Setup(service => service.UpdateBeer(It.IsAny<Beer>())).Callback<Beer>(beer =>
+            {
+                mockBeer.PromoPrice = beer.PromoPrice;
+            });
+
+            // Act
+            var result = _controller.UpdatePromoPrice(beerId, promoPriceUpdateRequest);
+
+            // Assert
+            _mockBeerService.Verify(service => service.UpdateBeer(It.Is<Beer>(b => b.PromoPrice == promoPriceUpdateRequest.NewPromoPrice)), Times.Once);
+            Assert.IsType<NoContentResult>(result);
+        }
     }
 }
